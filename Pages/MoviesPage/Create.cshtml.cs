5ee7fa3707572b15bd051687e19cf794b2ef3990
Anjllmi <mapg.delta77@gmail.com>
2021-07-06 21:00:32 -0500

    RazorPages  - OK Todos los tipos de tokes y login - OK
    Falta poblar Base de Datos de nuevo
    Falta integración de GraphQL

---
+++ Pages/MoviesPage/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Movies.Data;
+using Movies.Models;
+
+namespace Movies.Pages.MoviesPage
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Movies.Data.ApplicationDbContext _context;
+
+        public CreateModel(Movies.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["CategoryForeignKey"] = new SelectList(_context.Categories, "Id", "Name");
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movies.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
